e2b1ac7379f12715112efc765085b26d55372fd7
MP3-LECT <MP3-LECT@192.168.30.198>
2024-11-10 09:31:14 +0800

    This is my first commit for lab 4 question 1

---
+++ wva19004/src/Lab4/L4q1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MP3-LECT
+ */
+public class L4q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer");
+        int num1 = input.nextInt();
+        System.out.println("Enter second integer");
+        int num2 = input.nextInt();
+        int square1 = num1*num1;
+        int square2 = num2*num2;
+        int sumSquare = square1 + square2;
+        int diffSquare = square1 - square2;
+        System.out.println("The square of "+ num1+" is "+square1);
+        System.out.println("The square of "+ num2+" is "+square2);
+        System.out.println("The sum of the squares is " +sumSquare);
+        System.out.println("The differences of the squares is "+diffSquare);
+    }
+}
