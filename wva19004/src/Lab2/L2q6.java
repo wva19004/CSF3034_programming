14e3b6726fe80c3059eda060e51214b1817ca368
fdaus <fdaus.isa@gmail.com>
2024-12-22 02:00:13 +0800

    added lab 2 activity 6 java code

---
+++ wva19004/src/Lab2/L2q6.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab2;
+
+/**
+ *
+ * @author onewa
+ */
+public class L2q6 {
+
+    
+    public static void main(String[] args) {
+        // Display the table header
+        System.out.println("MPH\t\tKPH");
+        System.out.println("--------------------");
+
+        // Display speeds conversion
+        System.out.printf("%d\t\t%.2f\n", 60, 60 / 0.6214);
+        System.out.printf("%d\t\t%.2f\n", 70, 70 / 0.6214);
+        System.out.printf("%d\t\t%.2f\n", 80, 80 / 0.6214);
+        System.out.printf("%d\t\t%.2f\n", 90, 90 / 0.6214);
+        System.out.printf("%d\t\t%.2f\n", 100, 100 / 0.6214);
+        System.out.printf("%d\t\t%.2f\n", 110, 110 / 0.6214);
+        System.out.printf("%d\t\t%.2f\n", 120, 120 / 0.6214);
+        System.out.printf("%d\t\t%.2f\n", 130, 130 / 0.6214);
+    }   
+}
