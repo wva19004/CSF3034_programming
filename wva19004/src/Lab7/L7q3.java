6638c9f8166b5e40cc9c8bf6b961da98e051c0cc
fdaus <fdaus.isa@gmail.com>
2024-12-22 16:30:05 +0800

    added lab 7 activity 3

--- wva19004/src/Lab7/L7q3.java
+++ wva19004/src/Lab7/L7q3.java
@@ -11,64 +11,39 @@ import java.util.Scanner;
  * @author onewa
  */
 public class L7q3 {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number for the line pattern");
-        int line=6,n = input.nextInt();
+        boolean con = true;
+        do{
+            System.out.println("Enter a number for the line pattern");
+            int n= input.nextInt();
+            char space = ' ';
 
-        System.out.println("Pattern I\tPattern II\tPattern III\tPattern IV");
-        for (int i = 1,k=n; i <= n; i++,k--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j+" ");
-            }
-            
-            if(i<=n/2)
-                System.out.print("\t\t");
-            else
-                System.out.print("\t");
-            
-            for (int j = 1; j <=k; j++) {
-                System.out.print(j+" ");
-            }
-            
-            System.out.println("");
-        }
-        System.out.println("");
-        System.out.println("");
-        for(int i = 1;i<=line;i++){
-            for(int j = 1; j<=i;j++){
-                System.out.print(j+" ");
-            }
-            
-            if(i<=line/2)
-                System.out.print("\t\t");
-            else
-                System.out.print( "\t");            
+            if(n<1)
+                break;
+            System.out.printf("\nPattern I%-" + (n+1) + "sPattern II%-" + (n+1) + "sPattern III%-" + (n+1) + "sPattern IV\n",space,space,space);
+            for (int i = 1,k=n,p=n; i <= n; i++,k--,p-=2) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j+" ");
+                }
+                System.out.printf("%-" + (p+8) + "s", space); 
 
-            for(int k=line;k>=i;k--){
-                System.out.print(k+" ");
-            }
-            if(i<=line/2)
-                System.out.print("\t");
-            else
-                System.out.print("\t\t");
+                for (int j = 1; j <=k; j++) {
+                    System.out.print(j+" ");
+                }
+                System.out.printf("%-" + (n+8) + "s", space);
+                
+                for (int j = i; j > 0; j--) {
+                    System.out.print(j+" ");
+                }
+                System.out.printf("%-" + (p+8) + "s", space); 
 
-            for(int j = 1; j<=i;j++){
-                System.out.print(j+" ");
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j+" ");
+                }
+                System.out.println("");
             }
-            
-            if(i<=line/2)
-                System.out.print("\t\t");
-            else
-                System.out.print( "\t");
-
-            for(int k=line;k>=i;k--){
-                System.out.print(k+" ");
-            }
-            
-            System.out.println();
-
-        }
+        }while(con);
     }
 }
