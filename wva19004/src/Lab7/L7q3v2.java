8a25910532d168688ff3164e2988167ae6a3eb66
fdaus <fdaus.isa@gmail.com>
2024-12-22 16:29:51 +0800

    added lab 7 activity 3 v2

--- wva19004/src/Lab7/L7q3v2.java
+++ wva19004/src/Lab7/L7q3v2.java
@@ -26,7 +26,18 @@ public class L7q3v2 {
             System.out.println("");
         }
         
-        
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
         
     }
 }
