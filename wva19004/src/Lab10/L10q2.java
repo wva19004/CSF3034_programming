ca2052b3b7a8655412ab346d8275fd46b7a0916d
MP3-LECT <MP3-LECT@192.168.30.198>
2024-12-29 10:00:52 +0800

    add lab 10 question 2

---
+++ wva19004/src/Lab10/L10q2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author MP3-LECT
+ */
+public class L10q2 {
+    public static void main(String[] args) {
+        try{
+        File myFile = new File("new_input.txt");
+        Scanner scan = new Scanner(myFile);
+        
+        while(scan.hasNext()){
+        System.out.println(scan.nextLine());
+  
+        }
+        }catch(FileNotFoundException e){}
+    }
+}
